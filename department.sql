a8c6bbf74cb8458cedd0a10ccf9eeca6af3d9883
hasantanvirul <79777490+hasantanvirul@users.noreply.github.com>
2021-03-13 22:50:48 +0600

    Add files via upload

---
+++ department.sql
@@ -0,0 +1,64 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 13, 2021 at 05:39 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `department`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `depts`
+--
+
+CREATE TABLE `depts` (
+  `Department_id` int(4) NOT NULL,
+  `Department_Name` varchar(30) NOT NULL,
+  `Manager_id` int(6) DEFAULT NULL,
+  `Location_id` int(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `depts`
+--
+
+INSERT INTO `depts` (`Department_id`, `Department_Name`, `Manager_id`, `Location_id`) VALUES
+(10, 'Administration', 200, 1700),
+(20, 'Marketing', 201, 1800),
+(50, 'Shipping&', 124, 1500),
+(60, 'IT', 103, 1400),
+(80, 'Sales', 149, 2500),
+(90, 'Executive', 100, 1700),
+(110, 'Accounting', 205, 1700),
+(190, 'Contracting', NULL, 1700);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `depts`
+--
+ALTER TABLE `depts`
+  ADD PRIMARY KEY (`Department_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
